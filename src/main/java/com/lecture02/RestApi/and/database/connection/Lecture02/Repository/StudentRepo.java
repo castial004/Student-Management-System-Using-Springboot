90f5c8dd2017aff3d8555f2833113e563aa4c581
castial004 <nm6395825@gmail.com>
2026-01-26 10:05:40 +0530

    Integerated mysql database

--- src/main/java/com/lecture02/RestApi/and/database/connection/Lecture02/Repository/StudentRepo.java
+++ src/main/java/com/lecture02/RestApi/and/database/connection/Lecture02/Repository/StudentRepo.java
@@ -2,7 +2,9 @@ package com.lecture02.RestApi.and.database.connection.Lecture02.Repository;
 
 import com.lecture02.RestApi.and.database.connection.Lecture02.Entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepo extends JpaRepository<Student,Long> {
 
 }
