61903d008ca7c4819d54e858d31f41b7a8fe8a88
castial004 <nm6395825@gmail.com>
2026-01-28 15:43:04 +0530

    created a ENUM of type blood group for consistency

---
+++ src/main/java/com/lecture02/RestApi/and/database/connection/Lecture02/Entity/BloodGroup.java
@@ -0,0 +1,12 @@
+package com.lecture02.RestApi.and.database.connection.Lecture02.Entity;
+
+public enum BloodGroup {
+    A_Positive,
+    A_Negative,
+    B_Positive,
+    B_Negative,
+    AB_Positive,
+    AB_Negative,
+    O_Positive,
+    O_Negative;
+}
