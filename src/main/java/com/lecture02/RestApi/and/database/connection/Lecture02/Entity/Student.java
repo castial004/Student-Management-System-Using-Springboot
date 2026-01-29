c0a201d9beaab029cde4279534b38e89bbcfee7b
castial004 <nm6395825@gmail.com>
2026-01-29 19:09:49 +0530

    updated student entity

--- src/main/java/com/lecture02/RestApi/and/database/connection/Lecture02/Entity/Student.java
+++ src/main/java/com/lecture02/RestApi/and/database/connection/Lecture02/Entity/Student.java
@@ -1,17 +1,48 @@
 package com.lecture02.RestApi.and.database.connection.Lecture02.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@ToString
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//@Table(
+//        name = "Student",
+//        uniqueConstraints = {@UniqueConstraint(name = "some name",columnNames = {"name","birthDate"})},
+//        indexes = {@Index(name = "idx_student_email_cgpa",columnList = "email,cgpa")}
+//)
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
+    private double cgpa;
+
+    private LocalDate birthDate;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime lastUpdatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroup bloodGroup;
+
+    public Student(String name,String email){
+        this.name = name;
+        this.email = email;
+    }
 }
